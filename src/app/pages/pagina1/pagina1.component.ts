2440aabbaa415049731d8d5a1282bc1747c5685a
Isidro Sotoca <Isidre10_92@hotmail.es>
2022-04-15 17:19:49 +0200

    add an interval for test init and destroy

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -10,11 +11,15 @@ export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterConten
   AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   nombre: string = 'Isidro';
+  segundos: number = 0;
+
+  timerSubscription!: Subscription;
 
   constructor() {
     console.log('constructor');
   }
 
+  // Nota: este evento aqui no hace nada porque no tenemos ningun parametro input en el componente
   ngOnChanges(changes: SimpleChanges): void {
     console.log('ngOnChanges');
   }
@@ -41,10 +46,19 @@ export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterConten
 
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
+
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+
   }
 
   ngOnInit(): void {
     console.log('ngOnInit');
+
+    this.timerSubscription = interval(1000).subscribe( i => {
+      this.segundos++;
+    })
   }
 
   guardar() {
